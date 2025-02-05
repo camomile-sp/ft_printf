1b07edd671bf098b5f3c7c1355de9b215c53d7ee
Sasha Pavlova <sasha.pavlova15@gmail.com>
2025-02-05 22:45:15 +0100

    ft_puthex draft

--- ft_puthex.c
+++ ft_puthex.c
@@ -13,3 +13,23 @@
 #include "ft_printf.h"
 #include "libft/libft.h"
 
+static char ft_hexconvert(unsigned int n)
+{
+    char    *hex;
+    char    x;
+
+    hex = "0123456789abcdef";
+    x = hex[n];
+    return (x);
+}
+
+void    ft_puthex(unsigned int n, int *count)
+{
+    char    c;
+
+    if (n >= 16)
+        ft_puthex(n / 16);
+    c = ft_hexconvert(n % 16);
+    write(1, &c, 1);
+    (*count)++;
+}
